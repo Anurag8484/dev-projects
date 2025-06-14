f873db9c8899c132d66983debe3d3fa0f99922fc
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-14 13:36:40 +0530

    Add User course get logic

--- Course-fullstack/server/db.js
+++ Course-fullstack/server/db.js
@@ -8,26 +8,25 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: { type: String },
   name: { type: String },
-  role: { type: String, default: 'user' }
-
+  role: { type: String, default: "user" },
 });
 
 const adminSchema = new mongoose.Schema({
   // adminSchema here
   email: { type: String, unique: true },
   password: { type: String },
-  name: { type: String },
+  name: { type: String},
+  role: { type: String, default: "admin" },
 });
 
 const courseSchema = new mongoose.Schema({
 // courseSchema here
-id: { type: Number, unique:true },
+courseId: { type: Number, unique:true },
 title: { type: String, unique: true },
 description: { type: String },
 price: { type: Number },
 imageLink: { type: String },
-published: { type: Boolean},
-userId: { type: ObjectId}
+published: { type: Boolean, default: false},
 
 });
 const User = mongoose.model('User', userSchema);
--- Course-fullstack/server/server.js
+++ Course-fullstack/server/server.js
@@ -16,7 +16,6 @@ const secret = process.env.JWT_SECRERT;  // This should be in an environment var
 const port = process.env.PORT;
 
 // Connect to DB:
-
 async function connectToDB(){
     try {
         await mongoose.connect(process.env.MONGO_URI);
@@ -27,20 +26,37 @@ async function connectToDB(){
 
 connectToDB();
 
-const authMiddleware = (req, res, next) => {
+const adminAuth = (req, res, next) => {
 //  authMiddleware logic here 
-    const token = req.headers.token;
+    const token = req.headers.authorization;
+    console.log(token)
     const decodedData = jwt.verify(token,secret);
 
-    if (decodedData){
+    if (decodedData && decodedData.role === 'admin'){
+        req.id = decodedData.id;
+        req.role = decodedData.role
+        next();
+    }else{
+        res.status(403).json({
+            error: "Only Admins are allowed access."
+        });
+    };
+};
+const userAuth = (req, res, next) => {
+//  authMiddleware logic here 
+    const token = req.headers.authorization;
+    const decodedData = jwt.verify(token,secret);
+
+    if (decodedData && decodedData.role === 'user'){
         req.id = decodedData.id;
         next();
     }else{
         res.status(403).json({
-            error: "Incorrect Credentials!"
+            error: "Please login as User!"
         });
     };
 };
+
 
 
 // Admin routes
@@ -122,29 +138,179 @@ app.post('/admin/login', async (req, res) => {
 });
 
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', adminAuth, async(req, res) => {
     // logic to create a course
+
+    const id = req.id;
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+
+    try {
+        let courseCount = await Course.countDocuments();
+        console.log(courseCount)
+        await Course.create({
+            title: title,
+            description: description,
+            price: price,
+            imageLink: imageLink,
+            courseId: courseCount + 1
+        })
+        res.json({
+            message:`Course added successfully!`
+        });
+    } catch (error) {
+            console.error(`Error adding course to DB: ${error}`);
+            res.status(500).json({
+                error: `Failed to add course to DataBase`
+            })
+            return;
+    }
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+
+app.put('/admin/courses/:courseId',adminAuth, async(req, res) => {
     // logic to edit a course
+    const id = req.id;
+    const courseId = parseInt(req.params.courseId)
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+    const published = req.body.published;
+
+    try {
+        const updatedCourse = await Course.findOneAndUpdate(
+            {courseId: courseId},
+            {
+                title: title,
+                description: description,
+                price: price,
+                imageLink: imageLink,
+                published: published
+            },
+            {new: true}
+        );
+        res.json({
+            message:"Course edited!"
+        });
+    } catch (error) {
+            console.error(`Error editing course to DB: ${error}`);
+            res.status(500).json({
+                error: `Failed to edit course to DataBase`
+            })
+            return;
+    }
+
+
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get('/admin/courses', adminAuth, async(req, res) => {
     // logic to get all courses
+    try {
+        const courses = await Course.find();
+        if(courses.length == 0){
+            res.json({
+                message:"At present, there are 0 courses."
+            });
+            return;
+        }
+        res.json({
+            courses: courses
+        })            
+    } catch (error) {
+        console.error(`Error fetching courses from DB: ${error}`)
+        res.json({
+          message: `Error fetching courses from DB: ${error}`
+        });
+    }
+
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post('/users/signup', async(req, res) => {
     // logic to sign up user
+    const reqBody = z.object({
+        email: z.string().min(3).max(20).email(),
+        password: z.string().min(3).max(20),
+        name: z.string().min(3).max(20)
+    });
+
+    const safeParse = reqBody.safeParse(req.body);
+
+    if (!safeParse){
+        res.json({
+            error: safeParse.error.issues[0].message
+        });
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
+    const hashedPassword = await bcrypt.hash(password,3);
+    try{
+        await User.create({
+            email: email,
+            password: password,
+            name: name
+        });
+        res.json({
+            message: `User Signed Up!!`
+        })
+    }catch (err){
+        res.json({
+            db_error:err.errorResponse.errmsg,
+            error: `Error adding the user in the DB ${err}!`
+        });
+        return;
+    }
 });
 
-app.post('/users/login', (req, res) => {
+app.post('/users/login', async(req, res) => {
     // logic to log in user
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let user;
+    let passwordMatch;
+
+    try {
+        user = await User.find({
+            email: email
+        })
+
+        passwordMatch =  bcrypt.compare(password, user.password);
+    } catch (error) {
+        res.json({
+            error: `Error finding User in the DB: ${error}`
+        })
+    }
+
+    if(passwordMatch){
+        const token = jwt.sign({
+            id: user._id.toString()
+        },secret);
+        res.json({
+            message:`User Loged In.`,
+            token: token
+        });
+    }else{
+        res.json({
+            error: "Incorrect Credentials!"
+        });
+        return;
+    }
 });
 
-app.get('/users/courses', (req, res) => {
+app.get('/users/courses', async(req, res) => {
     // logic to list all courses
+
+
+
+
+
+
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
