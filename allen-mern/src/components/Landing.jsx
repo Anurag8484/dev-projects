e02f7cddd63dcce4613cea4f10b801aaf3b80fce
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-25 13:56:13 +0530

    allen header

---
+++ allen-mern/src/components/Landing.jsx
@@ -0,0 +1,17 @@
+import logo from '../assets/logo.svg'
+export function Landing(){
+    return(
+        <>
+            <header>
+                <div className="nav-left">
+                    <img src={logo}/>
+                </div>
+                <div className="nav-right"></div>
+            </header>
+
+
+
+
+        </>
+    )
+}
