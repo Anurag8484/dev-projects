58c0d181fb30122d2a1161f0af06abed796ad3af
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-25 15:56:59 +0530

    content img mad

--- allen-mern/src/components/Header.jsx
+++ allen-mern/src/components/Header.jsx
@@ -1,5 +1,8 @@
 import logo from "../assets/logo.svg";
 import banner1 from "../assets/banner1.png"
+import neet1 from "../assets/neet-1.png"
+import neet2 from "../assets/neet-2.png"
+import neet3 from "../assets/neet-1.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
@@ -28,19 +31,57 @@ export function Header() {
         <div className="header-banner">
           <p>
             <code>GET UP TO 90% SCHOLARSHIP </code>
-            on Neet repeater courses for 2026 
+            on Neet repeater courses for 2026
           </p>
         </div>
       </header>
 
       <section className="hero">
-
         <div className="hero-banner">
-            <img src={banner1} alt="" />
+          <img src={banner1} alt="" />
         </div>
-
       </section>
-      
+
+      <section className="home-content1">
+        <div className="content1">
+          <div className="top-content1">Pick the right course for you</div>
+          <div className="bottom-content1">
+            <div className="content1-card">
+              <div className="card-top">
+                <p>Neet Courses</p>
+              </div>
+              <div className="card-bottom1">
+                <div className="card-bottom-text">
+                  <p>View Courses</p>
+                </div>
+                <img src={neet1} alt="" />
+              </div>
+            </div>
+            <div className="content1-card">
+              <div className="card-top">
+                <p>JEE Courses</p>
+              </div>
+              <div className="card-bottom2">
+                <div className="card-bottom-text">
+                  <p>View Courses</p>
+                </div>
+                <img src={neet2} alt="" />
+              </div>
+            </div>
+            <div className="content1-card">
+              <div className="card-top">
+                <p>Courses for class 6-10</p>
+              </div>
+              <div className="card-bottom3">
+                <div className="card-bottom-text">
+                  <p>View Courses</p>
+                </div>
+                <img src={neet1} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
