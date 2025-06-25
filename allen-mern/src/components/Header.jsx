666121bb6772cd03f8d2473b03d39446b9aa9277
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-25 14:53:24 +0530

    Navbar done

--- allen-mern/src/components/Header.jsx
+++ allen-mern/src/components/Header.jsx
@@ -5,24 +5,33 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 export function Header() {
   return (
     <>
-      <div className="navbar">
-        <div className="nav-left">
-          <img src={logo} />
-          <p>Courses</p>
-          <p>Test Series</p>
-          <p>Results</p>
-          <p>Study Materials</p>
-          <p>Scholarships</p>
-          <p>Books</p>
-          <p>More</p>
-        </div>
-        <div className="nav-right">
-          <div className="phone-icon">
-            <FontAwesomeIcon icon={faPhone} />
+      <header>
+        <div className="navbar">
+          <div className="nav-left">
+            <img src={logo} />
+            <p>Courses</p>
+            <p>Test Series</p>
+            <p>Results</p>
+            <p>Study Materials</p>
+            <p>Scholarships</p>
+            <p>Books</p>
+            <p>More</p>
           </div>
-          <button>Login</button>
+          <div className="nav-right">
+            <div className="phone-icon">
+              <FontAwesomeIcon icon={faPhone} />
+            </div>
+            <button>Login</button>
+          </div>
         </div>
-      </div>
+        <div className="header-banner">
+          <p>
+            <code>GET UP TO 90% SCHOLARSHIP </code>
+            on Neet repeater courses for 2026 
+          </p>
+        </div>
+      </header>
+      
     </>
   );
 }
