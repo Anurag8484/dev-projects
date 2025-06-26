fda6d5093036f4b459afa3052f5fe9af1391e0b3
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-26 12:19:28 +0530

    course-frontend init

---
+++ coursify-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+
+export default function App(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element= {<Layout />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
---
+++ coursify-frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+export function Header(){
+
+    return(
+        <>
+            <header>
+                <div className="header-left">
+                    100xDevs
+                </div>
+                <div className="header-middle">
+                    <p>About</p>
+                    <p>Features</p>
+                    <p>News</p>
+                </div>
+                <div className="header-right">
+                    <button>Contact us!</button>                    
+                </div>
+            </header>
+
+        </>
+    )
+
+}
