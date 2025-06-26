2e8393df21dd54f40e05fcb8571498bba5c3342b
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-26 15:18:17 +0530

    Error msg styling and box re-style

--- coursify-frontend/src/components/Landing.jsx
+++ coursify-frontend/src/components/Landing.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { validation } from "../utils/validation";
 import "../landing.css";
 export function Landing() {
-    const [switchCard, setSwitchCard] = useState(true);
-    const [user, setUser] = useState({ name: "", email: "", password: "" });
-    const [errors, setErrors] = useState({ name: "", email: "", password: "" });
-    const [validCred, setValidCred] = useState(true);
-    const navigate = useNavigate();
+  const [switchCard, setSwitchCard] = useState(true);
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
+  const [errors, setErrors] = useState({ name: "", email: "", password: "" });
+  const [validCred, setValidCred] = useState(true);
+  const navigate = useNavigate();
 
   function HandleInputChange(event) {
     const { name, value } = event.target;
@@ -15,25 +16,28 @@ export function Landing() {
       ...p,
       [name]: value,
     }));
+
+    let errosCopy = { ...errors };
+    const errorR = validation(name, value, errosCopy);
+    console.log(errorR)
+    setErrors(errorR);
   }
 
   async function Signup() {
     try {
-      const res = await axios.post(
-        "http://localhost:3001/users/signup",
-           {
-            name: user.name,
-            email: user.email,
-            password: user.password,
-          },
-        
-      );
-      if (res.status){
-        navigate("/home")
-      }else{
-
+      const res = await axios.post("http://localhost:3001/users/signup", {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
+      if (res.status) {
+        setUser({name:'',password:'',email:''})
+        navigate("/home");
+      } else {
       }
-    } catch (error) {}
+    } catch (error) {
+        console.error(error)
+    }
   }
   async function Signin() {}
 
@@ -46,40 +50,47 @@ export function Landing() {
               <h4>SignUp</h4>
             </div>
             <div className="card-content">
-              <p>Name</p>
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                onChange={HandleInputChange}
-              />
-              <p>Email</p>
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                onChange={HandleInputChange}
-              />
-              <p>Password</p>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={HandleInputChange}
-              />
-            </div>
-            <div className="card-footer">
-              <button onClick={Signup}>SignUp</button>
-              <p>
-                Already Registered!{" "}
-                <span onClick={() => setSwitchCard(false)}>Login</span>
-              </p>
+              <div className="formField">
+                <p>Name</p>
+                <div className="inputField">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onChange={HandleInputChange}
+                  />
+                  <small>{errors.name}</small>
+                </div>
+                <p>Email</p>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  onChange={HandleInputChange}
+                />
+                <small>{errors.email}</small>
+                <p>Password</p>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={HandleInputChange}
+                  />
+                  <small>{errors.password}</small>
+              </div>
+              <div className="card-footer">
+                <button onClick={Signup}>SignUp</button>
+                <p>
+                  Already Registered!{" "}
+                  <span onClick={() => setSwitchCard(false)}>Login</span>
+                </p>
 
-              {!validCred ? (
-                <div className="errorDiv">Invalid Credentials</div>
-              ) : (
-                ""
-              )}
+                {!validCred ? (
+                  <div className="errorDiv">Invalid Credentials</div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         </section>
@@ -94,32 +105,37 @@ export function Landing() {
               <h4>SignIn</h4>
             </div>
             <div className="card-content">
-              <p>Email</p>
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                onChange={HandleInputChange}
-              />
-              <p>Password</p>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={HandleInputChange}
-              />
-            </div>
-            <div className="card-footer">
-              <button>SignIn</button>
-              <p>
-                New User?{" "}
-                <span onClick={() => setSwitchCard(true)}>SignUp</span>
-              </p>
-              {!validCred ? (
-                <div className="errorDiv">Invalid Credentials</div>
-              ) : (
-                ""
-              )}
+              <div className="formField">
+                <p>Email</p>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  onChange={HandleInputChange}
+                />
+                <small>{errors.email}</small>
+                <p>Password</p>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={HandleInputChange}
+                />
+                <small>{errors.password}</small>
+              </div>
+              <div className="card-footer">
+                <button onClick={Signin}>SignIn</button>
+                <p>
+                  New User!{" "}
+                  <span onClick={() => setSwitchCard(true)}>Register</span>
+                </p>
+
+                {!validCred ? (
+                  <div className="errorDiv">Invalid Credentials</div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         </section>
