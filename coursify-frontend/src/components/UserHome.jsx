677f153ce7a7e333107244dca7806b27970ce72a
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-27 16:06:58 +0530

    COMPLETE Admin side logic

--- coursify-frontend/src/components/UserHome.jsx
+++ coursify-frontend/src/components/UserHome.jsx
@@ -1,68 +1,137 @@
 import { useEffect,useState } from "react"
+import { useNavigate } from "react-router-dom";
 import axios from "axios"
+
 import "../userHome.css"
 export function UserHome(){
     const[courses,setCourses] = useState([])
+    const[purchasedCourses,setPurchasedCourses] = useState([])
     const [error,setError] = useState("")
     const [loading,setLoading] = useState(true);
-    useEffect(()=>{
-        const fetchCourses = async()=>{
-            try {
-                const token = localStorage.getItem('token')
-                console.log("checkpoint 1")
-                const res = await axios.get(
-                    "http://localhost:3001/users/courses",
-                    {
-                        headers: {
-                            authorization: token
-                        },
-                    }
-                );
-                console.log("checkpoint 2")
-                console.log(res)
-                if(res.status){
-                    console.log("res ok")
-                    setCourses(res.data.courses);
-                }else{
-                    setError("Currently there are 0 courses.");
-                }
-            } catch (error) {
-                setError(error.response?.data?.error || error.message);
-            }finally{
-                setLoading(false)
-            }
-        };
+    const[viewPurchased,setViewPurchased] = useState(false)
+    const navigate = useNavigate();
 
-        fetchCourses();
-    },[]);
+    async function purchaseCourse(courseId){
+        try {
+          const token = localStorage.getItem("token");
+          console.log(token);
+          console.log("check 1");
+          
+          const res = await axios.post(
+            `http://localhost:3001/users/courses/${courseId}`,{},
+            {
+              headers: {
+                authorization: token,
+              },
+            }
+          );
+          if (res.status) {
+            console.log(res);
+            
+            fetchCourses();
+          } else {
+            setError("Currently there are 0 courses.");
+          }
+        } catch (error) {
+          setError(error.response?.data?.error || error.message);
+        } finally {
+          setLoading(false);
+        }
+    }
+ 
+
+
+    const fetchCourses = async () => {
+      const token = localStorage.getItem("token");
+      try {
+          const[coursesRes, purchasedRes] = await Promise.all([
+              axios.get("http://localhost:3001/users/courses", {headers: { authorization: token,},}),
+              axios.get("http://localhost:3001/users/purchasedCourses", {headers: { authorization: token,},})
+          ])
+          const courseAll = coursesRes.data.courses ?? [];
+          const pruchaseAll = purchasedRes.data.purchasedCourses ?? [];
+
+          setCourses(courseAll)
+          setPurchasedCourses(pruchaseAll)
+       
+      } catch (error) {
+        setError(error.response?.data?.error || error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    useEffect(() => {
+
+      fetchCourses();
+    }, []);
+
+    const purchasedIds = new Set(purchasedCourses.map(c=>c.courseId));
 
 
     return (
       <>
         <div className="pageTitle">
           <h2>Courses Available</h2>
-        </div>
-
-        <div className="course-container">
-          {(loading) ? (
-            <h3>Loading.....</h3>
+          {viewPurchased ? (
+            <button onClick={() => setViewPurchased(false)}>
+              View All Courses
+            </button>
           ) : (
-            courses.map((course)=>(
-
-                
-                <div key={course.courseId} className="courseCard">
-                <h4>Name: {course.title}</h4>
-                <p>Description: {course.description}</p>
-              <p>Price: {course.price}</p>
-              <div className="cardFooter">
-                <button className="edit">Edit</button>
-                <button className="delete">Delete</button>
-              </div>
-            </div>
-        ))
-            
+            <button onClick={() => setViewPurchased(true)}>
+              View Purchased Courses
+            </button>
           )}
         </div>
+
+        {viewPurchased ? (
+          <div className="course-container">
+            {loading ? (
+              <h3>Loading.....</h3>
+            ) : (
+              purchasedCourses.map((course) => (
+                <div key={course.courseId} className="courseCard">
+                  <h4>Name: {course.title}</h4>
+                  <p>Description: {course.description}</p>
+                  <p>Price: {course.price}</p>
+                  <div>
+                    {purchasedIds.has(course.courseId) ? (
+                      <p className="coursePurchase">Purchased</p>
+                    ) : (
+                      <button onClick={() => purchaseCourse(course.courseId)}>
+                        Purchase
+                      </button>
+                    )}
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        ) : (
+          <div className="course-container">
+            {loading ? (
+              <h3>Loading.....</h3>
+            ) : (
+              courses.map((course) => (
+                <div key={course.courseId} className="courseCard">
+                  <h4>Name: {course.title}</h4>
+                  <p>Description: {course.description}</p>
+                  <p>Price: {course.price}</p>
+                  <div>
+                    {purchasedIds.has(course.courseId) ? (
+                      <p className="coursePurchase">Purchased</p>
+                    ) : (
+                      <button onClick={() => purchaseCourse(course.courseId)}>
+                        Purchase
+                      </button>
+                    )}
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        )}
+
+       
       </>
     );
 }
