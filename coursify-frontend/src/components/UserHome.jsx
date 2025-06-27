ee5abc14542fd1f2a81e5d75218d82dfc84c937a
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-27 14:42:29 +0530

    user course show ui

--- coursify-frontend/src/components/UserHome.jsx
+++ coursify-frontend/src/components/UserHome.jsx
@@ -1,10 +1,24 @@
+import "../userHome.css"
 export function UserHome(){
     return(
         <>
-        <h2>User Home</h2>
-        <div>Courses</div>
-        <div className="courses list">
-            
+        <div className="pageTitle">
+        <h2>Courses Available</h2>
+        </div>
+        <div className="course-container">
+        <div className="courseCard">
+            <h4>Name: MAD</h4>
+            <p>asdd: asdasd</p>
+            <p>asd: ad</p>
+            <p>asdasd: asdasd</p>
+            <div className="cardFooter">
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+            </div>
+        </div>
+        
+     
+
         </div>
         </>
     )
