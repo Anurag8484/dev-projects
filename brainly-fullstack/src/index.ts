9b3b72ee44003ded07571b3baf7eb11bd84268cc
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-31 09:31:43 +0530

    implement routes

--- brainly-fullstack/src/index.ts
+++ brainly-fullstack/src/index.ts
@@ -1,5 +1,32 @@
 
-import express from 'express';
+import express, { Express } from 'express';
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
-const app = express();
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+const mongoDB_uri:string = process.env.MONGODB_URI || ''
+async function connectDB(): Promise<void>{
+
+    try {
+        await mongoose.connect(mongoDB_uri);
+        console.log("Mongoose Connected")
+    } catch (error) {
+        console.error("Error connecting to the DB",error);
+    }
+
+}
+
+const app: Express = express();
+app.use(express.json());
+
+
+
+function main(): void{
+    connectDB();
+    app.listen(port)
+    console.log(`listening on the port ${port}`);
+}
+
+main();
